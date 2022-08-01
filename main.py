791a450349b0fb7083821e3cd2838f166a6727aa
Nickname <47962021+nicknamekr@users.noreply.github.com>
2022-08-01 16:18:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from discord.ext import commands
 import os, discord
 from dotenv import load_dotenv
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 load_dotenv(verbose=True)
-bot = commands.Bot(command_prefix='ㅅ')
+bot = commands.Bot(command_prefix=["ㅅ", "샍", "샌즈!"])
 
 @bot.event
 async def on_ready():
@@ -16,6 +19,10 @@ async def sans(ctx):
 @bot.command(name = "와", alias=["와!", "wa!", "wha!", "wa", "wha"])
 async def wa(ctx):
   await ctx.reply('와! 샌즈!\nhttps://imgur.com/a/6eVnYAd')
+  
+@bot.command(name = "핑" alias=["ping"])
+async def ping(ctx):
+    await ctx.reply(f'{round(bot.latency * 1000)}ms') 
 
 print('커멘드 로딩 완료!')
 
